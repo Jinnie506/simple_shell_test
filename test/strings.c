2e94d78e5dd30888aa2e656b0a721020f26820c0
Ifeoma <ojiniifeoma@gmail.com>
2021-08-19 14:58:34 -0700

    Test repository

---
+++ test/strings.c
@@ -0,0 +1,65 @@
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+
+
+int _strlen(const char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
